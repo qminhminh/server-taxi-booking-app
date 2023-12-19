446b4a5929f06482a783e7ccaf4992b711d7012f
qminhminh <hqminh050503@gmail.com>
2023-12-19 18:06:27 +0700

    middlewware admin and user

---
+++ server/middlewares/admin.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const Customer = require('../models/customer_model');
+
+
+const admin = async( req, res,next) => {
+    try{
+        // check x auth token
+        const token = req.header('x-auth-token') ;
+        if(!token) return res.status(401).json({msg: 'NO auth token'});
+
+        // check password
+        const verified= jwt.verify(token,'passwordKey');
+        if(!verified) return res.status(401).json({msg: 'Token verification failed, auth denied!'});
+        
+        // check user and seller
+        const user = await Customer.findById(verified.id);
+        if(user.type == 'user' || user.type == 'seller'){
+            return res.status(401).json({msg: 'You are not a Admin'});
+        }
+        req.user = verified.id;
+        req.token = token;
+        next();
+     
+    }catch(e){
+        res.status(500).json({error: e.message});
+    }
+}
+
+module.exports = admin;
--- server/routers/token.js
+++ server/routers/token.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { tokenIsvalidCustomer, getUserTokenCustomer } = require('../controller/tokenController');
+const auth = require('../middlewares/auth');
 const tokenRouter = express.Router();
 
 // token users customer
 tokenRouter.post("/tokenisvalidcustomer", tokenIsvalidCustomer);
-tokenRouter.get("/", getUserTokenCustomer);
+tokenRouter.get("/",auth, getUserTokenCustomer);
 
 module.exports = tokenRouter;
